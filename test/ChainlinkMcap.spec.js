396d7745c51c98a16aef4ab9edcef6df2675bab4
d1ll0n <dillonkellar@protonmail.com>
2021-02-19 06:38:51 -0600

    Updated test file to cover more of the contract and use a minimal mock file.

--- test/ChainlinkMcap.spec.js
+++ test/ChainlinkMcap.spec.js
@@ -1,184 +1,308 @@
 const { expect } = require('chai');
-const { waffle } = require('hardhat');
-const { deployContract, solidity } = waffle;
-const provider = waffle.provider;
+const { BigNumber } = require('ethers');
+const {
+  ChainlinkJobId,
+  OracleAddress,
+  AaveAddress,
+  SnxAddress,
+  MinimumDelay,
+  TimeToExpire,
+  sha3,
+  deploy,
+  getTransactionTimestamp,
+  fastForward
+} = require('./utils');
 
-const ChainlinkJobId = '6d1bfe27e7034b1d87b5270556b17277';
-const OracleAddress = '0x7AFe1118Ea78C1eae84ca8feE5C65Bc76CcF879e';
 
-const AaveAddress = '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9';
-const SnxAddress = '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f';
-const LinkAddress = '0x514910771af9ca656af840dff83e8264ecf986ca';
+describe('ChainlinkMcap', function () {
+  let chainlinkContract;
+  let beforeupdaterequesttimestamp;
+  let link;
 
-describe('ChainlinkMcap', function(){
-    let chainlinkContract;
-    let beforeupdaterequesttimestamp;
+  let owner, addr1, addr2, addr3, addr4;
 
-    let owner, addr1, addr2, addr3, addr4;
+  before(async () => {
+    [owner, addr1, addr2, addr3, addr4] = await ethers.getSigners();
+  });
 
+  function setupTests() {
     before(async () => {
-        [owner, addr1, addr2, addr3, addr4] = await ethers.getSigners();
-
-        chainlinkContract = await ethers.getContractFactory('ChainlinkMcapMock');
-        chainlinkContract = await chainlinkContract.deploy(0, 86400, OracleAddress, ethers.utils.toUtf8Bytes(ChainlinkJobId));
-    });
-
-    it('should be able to find the deployed contract', async function(){
-        let name = await chainlinkContract.oracle();
-        expect(name).to.equal(OracleAddress);
-    });
-
-    describe('updateCirculatingMarketCaps()', function(){
-        it('should revert when updating non-whitelisted addresses', async function(){
-            expect(chainlinkContract.updateCirculatingMarketCaps([AaveAddress])).to.be.reverted;
-        });
-
-        it('should allow updating of whitelisted addresses', async function(){
-            // Assert that both tokens are not whitelisted
-            expect(chainlinkContract.updateCirculatingMarketCaps([AaveAddress])).to.be.reverted;
-            expect(chainlinkContract.updateCirculatingMarketCaps([SnxAddress])).to.be.reverted;
-
-            // Whitelist and attempt to update SNX
-            await chainlinkContract.addTokensToWhitelist([SnxAddress]);
-            await chainlinkContract.updateCirculatingMarketCaps([SnxAddress]);
-
-            // Assert that AAVE is still not whitelisted
-            expect(chainlinkContract.updateCirculatingMarketCaps([AaveAddress])).to.be.reverted;
-        });
-
-        it('should revert if minDelay has not been reached', async function(){
-          await chainlinkContract.addTokensToWhitelist([SnxAddress]);
-
-          await chainlinkContract.setMinimumDelay(1000);
-          await chainlinkContract.fulfillMock(SnxAddress, 1337);
-
-          expect(chainlinkContract.updateCirculatingMarketCaps([SnxAddress])).to.be.reverted;
-
-          // reset for future tests
-          await chainlinkContract.setMinimumDelay(0);
-          await chainlinkContract.fulfillMock(SnxAddress, 0);
-        });
-
-        it('should remove token from whitelist if requested', async function(){
-
-          expect(chainlinkContract.updateCirculatingMarketCaps([SnxAddress])).to.be.reverted;
-
-          await chainlinkContract.addTokensToWhitelist([SnxAddress]);
-          await chainlinkContract.updateCirculatingMarketCaps([SnxAddress]);
-          await chainlinkContract.removeTokensFromWhitelist([SnxAddress]);
-
-          expect(chainlinkContract.updateCirculatingMarketCaps([SnxAddress])).to.be.reverted;
-        });
-    });
-
-    describe('getCirculatingMarketCap()', function(){
-      it('should fetch the latest marketcap', async function(){
-        expect(chainlinkContract.getCirculatingMarketCap([SnxAddress])).to.be.reverted;
-        await chainlinkContract.addTokensToWhitelist([SnxAddress]);
-
-        let marketcap = await chainlinkContract.getCirculatingMarketCap(SnxAddress);
-        expect(marketcap).to.equal(0);
-
-        await chainlinkContract.fulfillMock(SnxAddress, 1337);
-
-        marketcap = await chainlinkContract.getCirculatingMarketCap(SnxAddress);
-        expect(marketcap).to.equal(1337);
-
-        // reset for future tests
-        await chainlinkContract.fulfillMock(SnxAddress, 0);
-        await chainlinkContract.removeTokensFromWhitelist([SnxAddress]);
-      });
-
-      it('should revert when marketcap is expired', async function(){
-        await chainlinkContract.fulfillMock(SnxAddress, 1337);
-        await chainlinkContract.setTimeToExpire(0);
-        expect(chainlinkContract.getCirculatingMarketCap([SnxAddress])).to.be.reverted;
-
-        // reset test
-        await chainlinkContract.fulfillMock(SnxAddress, 0);
-        await chainlinkContract.setTimeToExpire(86400);
-      });
-    });
-
-    describe('getCirculatingMarketCaps()', function(){
-      it('should fetch the latest marketcaps', async function(){
-        expect(chainlinkContract.getCirculatingMarketCap([AaveAddress, SnxAddress])).to.be.reverted;
-        await chainlinkContract.addTokensToWhitelist([AaveAddress, SnxAddress]);
-
-        await chainlinkContract.fulfillMock(AaveAddress, 0);
-        await chainlinkContract.fulfillMock(SnxAddress, 1337);
-
-        marketcaps = await chainlinkContract.getCirculatingMarketCaps([AaveAddress, SnxAddress]);
-        expect(marketcaps[0]).to.equal(0);
-        expect(marketcaps[1]).to.equal(1337);
-
-        // reset for future tests
-        await chainlinkContract.fulfillMock(SnxAddress, 0);
-        await chainlinkContract.removeTokensFromWhitelist([AaveAddress, SnxAddress]);
-      });
-
-      it('should revert when marketcap is expired', async function(){
-        await chainlinkContract.fulfillMock(AaveAddress, 1337);
-        await chainlinkContract.fulfillMock(SnxAddress, 1337);
-        await chainlinkContract.setTimeToExpire(0);
-
-        expect(chainlinkContract.getCirculatingMarketCap([AaveAddress, SnxAddress])).to.be.reverted;
-        expect(chainlinkContract.getCirculatingMarketCap([SnxAddress])).to.be.reverted;
-        expect(chainlinkContract.getCirculatingMarketCap([AaveAddress])).to.be.reverted;
-
-        // reset test
-        await chainlinkContract.fulfillMock(SnxAddress, 0);
-        await chainlinkContract.fulfillMock(AaveAddress, 0);
-        await chainlinkContract.setTimeToExpire(86400);
-      });
-    });
-
-    describe('minimumDelay()', function(){
-      it('should have the right minDelay set', async function(){
-        let minDelay = await chainlinkContract.minimumDelay();
-        expect(minDelay).to.equal(0);
-      });
-
-      it('should be able to modify minDelay', async function(){
-        await chainlinkContract.setMinimumDelay(5);
-        let minDelay = await chainlinkContract.minimumDelay();
-        expect(minDelay).to.equal(5);
-
-        // reset minDelay to 0
-        await chainlinkContract.setMinimumDelay(0);
-
-      });
-
-      it('should not be able to modify minDelay as non-owner', async function(){
-        expect(chainlinkContract.connect(addr1).setTimeToExpire(5)).to.be.reverted;
-      });
-    });
-
-    describe('setTimeToExpire()', function(){
-      it('should have the right timeToExpire set', async function(){
-        let timeToExpire = await chainlinkContract.timeToExpire();
-        expect(timeToExpire).to.equal(86400);
-      });
-
-      it('should be able to modify timeToExpire', async function(){
-        await chainlinkContract.setTimeToExpire(5);
-        let timeToExpire = await chainlinkContract.timeToExpire();
-        expect(timeToExpire).to.equal(5);
-
-        // reset timeToExpire to 86400
-        await chainlinkContract.setTimeToExpire(86400);
-      });
-
-      it('should not be able to modify timeToExpire as non-owner', async function(){
-        expect(chainlinkContract.connect(addr1).setTimeToExpire(5)).to.be.reverted;
-      });
-    });
-
-
-    describe('withdraw()', function(){
-      it('should allow only owners to withdraw', async function(){
-        expect(chainlinkContract.connect(addr1).withdrawLink()).to.be.reverted;
-        await chainlinkContract.withdrawLink();
-      })
+      link = await deploy('MockERC20', 'Chainlink', 'LINK');
+      chainlinkContract = await deploy(
+        'ChainlinkMcapMock',
+        MinimumDelay,
+        TimeToExpire,
+        OracleAddress,
+        ethers.utils.toUtf8Bytes(ChainlinkJobId),
+        link.address
+      );
     })
+  }
+
+  describe('Settings', () => {
+    setupTests();
+    
+    it('oracle()', async () => {
+      expect(await chainlinkContract.oracle()).to.eq(OracleAddress);
+    })
+
+    it('minimumDelay()', async () => {
+      expect(await chainlinkContract.minimumDelay()).to.eq(MinimumDelay)
+    })
+
+    it('timeToExpire()', async () => {
+      expect(await chainlinkContract.timeToExpire()).to.eq(TimeToExpire)
+    })
+
+    it('oracle()', async () => {
+      expect(await chainlinkContract.oracle()).to.eq(OracleAddress)
+    })
+
+    it('jobId()', async () => {
+      expect(await chainlinkContract.jobId()).to.eq('0x' + Buffer.from(ethers.utils.toUtf8Bytes(ChainlinkJobId)).toString('hex'));
+    })
+
+    it('fee()', async () => {
+      expect(await chainlinkContract.fee()).to.eq(1e17);
+    })
+  })
+
+  describe('getCoingeckoMarketCapUrl()', () => {
+    it('should compute the correct URL', async () => {
+      await chainlinkContract.testUrlDerivation();
+    })
+  })
+
+  describe('updateCirculatingMarketCaps()', function () {
+    setupTests();
+
+    it('should revert when updating non-whitelisted addresses', async function () {
+      expect(chainlinkContract.updateCirculatingMarketCaps([AaveAddress])).to.be.revertedWith(
+        "ChainlinkMcap: Token is not whitelisted"
+      );
+      expect(chainlinkContract.updateCirculatingMarketCaps([SnxAddress])).to.be.revertedWith(
+        "ChainlinkMcap: Token is not whitelisted"
+      );
+    });
+
+    it('should allow updating of whitelisted addresses', async function () {
+      // Whitelist and attempt to update SNX
+      await chainlinkContract.addTokensToWhitelist([SnxAddress, AaveAddress]);
+      await chainlinkContract.updateCirculatingMarketCaps([SnxAddress, AaveAddress]);
+    });
+
+    it('should mark the tokens as pending', async () => {
+      const { hasPendingRequest: hasPendingRequest1 } = await chainlinkContract.getTokenDetails(SnxAddress);
+      const { hasPendingRequest: hasPendingRequest2 } = await chainlinkContract.getTokenDetails(AaveAddress);
+      expect(hasPendingRequest1).to.be.true;
+      expect(hasPendingRequest2).to.be.true;
+    })
+
+    it('should save the token for the request ID', async () => {
+      expect(
+        await chainlinkContract.pendingRequestMap(sha3(SnxAddress))
+      ).to.eq(SnxAddress)
+      expect(
+        await chainlinkContract.pendingRequestMap(sha3(AaveAddress))
+      ).to.eq(AaveAddress)
+    })
+
+    it('should fail gracefully if minDelay has not been reached', async function () {
+      // Set minimum delay
+      await chainlinkContract.setMinimumDelay(1000);
+      expect((await chainlinkContract.getTokenDetails(SnxAddress)).hasPendingRequest).to.be.true;
+      // Fill pending request
+      await chainlinkContract.fulfill(sha3(SnxAddress), 1337);
+      expect((await chainlinkContract.getTokenDetails(SnxAddress)).hasPendingRequest).to.be.false;
+      // Verify no new request is made
+      await chainlinkContract.updateCirculatingMarketCaps([SnxAddress]);
+      expect((await chainlinkContract.getTokenDetails(SnxAddress)).hasPendingRequest).to.be.false;
+      expect(
+        await chainlinkContract.pendingRequestMap(sha3(SnxAddress))
+      ).to.eq(`0x`.padEnd(42, '0'));
+    });
+  });
+
+  describe('addTokensToWhitelist()', () => {
+    setupTests();
+
+    it('should not be able to whitelist tokens as non-owner', async function () {
+      expect(
+        chainlinkContract.connect(addr1).addTokensToWhitelist([SnxAddress, AaveAddress])
+      ).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it('should add tokens to whitelist if called by owner', async () => {
+      await chainlinkContract.addTokensToWhitelist([SnxAddress, AaveAddress]);
+      expect(await chainlinkContract.isTokenWhitelisted(SnxAddress)).to.be.true;
+      expect(await chainlinkContract.isTokenWhitelisted(AaveAddress)).to.be.true;
+    })
+  })
+
+  describe('removeTokensFromWhitelist()', () => {
+    setupTests();
+
+    it('should not be able to dewhitelist tokens as non-owner', async function () {
+      expect(
+        chainlinkContract.connect(addr1).removeTokensFromWhitelist([SnxAddress, AaveAddress])
+      ).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it('should remove tokens from whitelist if called by owner', async () => {
+      await chainlinkContract.addTokensToWhitelist([SnxAddress, AaveAddress]);
+      expect(await chainlinkContract.isTokenWhitelisted(SnxAddress)).to.be.true;
+      expect(await chainlinkContract.isTokenWhitelisted(AaveAddress)).to.be.true;
+      await chainlinkContract.removeTokensFromWhitelist([SnxAddress, AaveAddress]);
+      expect(await chainlinkContract.isTokenWhitelisted(SnxAddress)).to.be.false;
+      expect(await chainlinkContract.isTokenWhitelisted(AaveAddress)).to.be.false;
+    })
+  })
+
+  describe('fulfill()', () => {
+    setupTests();
+
+    it('should revert if market cap exceeds uint208', async () => {
+      await chainlinkContract.addTokensToWhitelist([SnxAddress]);
+      await chainlinkContract.updateCirculatingMarketCaps([SnxAddress]);
+      const amount = BigNumber.from(2).pow(208);
+      await expect(
+        chainlinkContract.fulfill(sha3(SnxAddress), amount)
+      ).to.be.revertedWith('ChainlinkMcap: uint exceeds 208 bits');
+    })
+
+    it('should mark latest timestamp, update market cap, remove pending request', async () => {
+      const tx = await chainlinkContract.fulfill(sha3(SnxAddress), 1337);
+      const {
+        hasPendingRequest,
+        lastPriceTimestamp,
+        marketCap
+      } = await chainlinkContract.getTokenDetails(SnxAddress);
+      expect(hasPendingRequest).to.be.false;
+      expect(lastPriceTimestamp).to.eq(await getTransactionTimestamp(tx));
+      expect(marketCap).to.eq(1337);
+      expect(
+        await chainlinkContract.pendingRequestMap(sha3(SnxAddress))
+      ).to.eq(`0x`.padEnd(42, '0'));
+    })
+  })
+
+  describe('getCirculatingMarketCap()', function () {
+    setupTests();
+
+    it('should revert if market cap does not exist', async () => {
+      expect(
+        chainlinkContract.getCirculatingMarketCap(SnxAddress)
+      ).to.be.revertedWith('ChainlinkMcap: Marketcap has expired');
+    })
+
+    it('should fetch the latest market cap', async function () {
+      await chainlinkContract.addTokensToWhitelist([SnxAddress]);
+      await chainlinkContract.updateCirculatingMarketCaps([SnxAddress]);
+      await chainlinkContract.fulfill(sha3(SnxAddress), 1337);
+
+      expect(
+        await chainlinkContract.getCirculatingMarketCap(SnxAddress)
+      ).to.eq(1337);
+    });
+
+    it('should revert when market cap is expired', async function () {
+      await fastForward(TimeToExpire);
+      expect(
+        chainlinkContract.getCirculatingMarketCap(SnxAddress)
+      ).to.be.revertedWith('ChainlinkMcap: Marketcap has expired');
+    });
+  });
+
+  describe('getCirculatingMarketCaps()', function () {
+    setupTests();
+
+    it('should revert if any market cap does not exist', async () => {
+      expect(
+        chainlinkContract.getCirculatingMarketCaps([SnxAddress, AaveAddress])
+      ).to.be.revertedWith('ChainlinkMcap: Marketcap has expired');
+    })
+
+    it('should fetch the latest market caps', async function () {
+      await chainlinkContract.addTokensToWhitelist([SnxAddress, AaveAddress]);
+      await chainlinkContract.updateCirculatingMarketCaps([SnxAddress, AaveAddress]);
+      await chainlinkContract.fulfill(sha3(SnxAddress), 1337);
+      await chainlinkContract.fulfill(sha3(AaveAddress), 9999);
+      const [snxCap, aaveCap] = await chainlinkContract.getCirculatingMarketCaps([SnxAddress, AaveAddress]);
+      expect(snxCap).to.eq(1337);
+      expect(aaveCap).to.eq(9999);
+    });
+
+    it('should revert when any market cap is expired', async function () {
+      await fastForward(TimeToExpire);
+      expect(
+        chainlinkContract.getCirculatingMarketCaps([SnxAddress, AaveAddress])
+      ).to.be.revertedWith('ChainlinkMcap: Marketcap has expired');
+    });
+  });
+
+  describe('setMinimumDelay()', function () {
+    it('should not be able to modify minDelay as non-owner', async function () {
+      expect(
+        chainlinkContract.connect(addr1).setMinimumDelay(5)
+      ).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it('should be able to modify minimumDelay as owner', async function () {
+      await chainlinkContract.setMinimumDelay(5);
+      expect(await chainlinkContract.minimumDelay()).to.equal(5);
+    });
+  });
+
+  describe('setTimeToExpire()', function () {
+    it('should not be able to modify timeToExpire as non-owner', async function () {
+      expect(
+        chainlinkContract.connect(addr1).setTimeToExpire(5)
+      ).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it('should be able to modify timeToExpire as owner', async function () {
+      await chainlinkContract.setTimeToExpire(5);
+      expect(await chainlinkContract.timeToExpire()).to.equal(5);
+    });
+  });
+
+  describe('setTimeToExpire()', function () {
+    it('should not be able to modify timeToExpire as non-owner', async function () {
+      expect(
+        chainlinkContract.connect(addr1).setTimeToExpire(5)
+      ).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it('should be able to modify timeToExpire as owner', async function () {
+      await chainlinkContract.setTimeToExpire(5);
+      expect(await chainlinkContract.timeToExpire()).to.equal(5);
+    });
+  });
+
+  describe('withdrawLink()', function () {
+    it('should not be able to withdraw as non-owner', async function () {
+      expect(
+        chainlinkContract.connect(addr1).withdrawLink()
+      ).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it('should be able to withdraw as owner', async function () {
+      await link.getFreeTokens(chainlinkContract.address, 1000);
+      await chainlinkContract.withdrawLink();
+      expect(await link.balanceOf(owner.address)).to.eq(1000);
+    })
+  })
+
+  describe('setChainlinkNodeFee()', function () {
+    it('should not be able to set fee as non-owner', async function () {
+      expect(
+        chainlinkContract.connect(addr1).setChainlinkNodeFee(10000)
+      ).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it('should be able to withdraw as owner', async function () {
+      await chainlinkContract.setChainlinkNodeFee(10000);
+      expect(await chainlinkContract.fee()).to.eq(1000);
+    })
+  })
 });
