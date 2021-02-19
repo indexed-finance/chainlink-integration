11cb1dc1342a7de5e15b31e57adf614e7914f1d7
d1ll0n <dillonkellar@protonmail.com>
2021-02-19 06:42:29 -0600

    add isTokenWhitelisted to interface

--- contracts/interfaces/ICirculatingMarketCapOracle.sol
+++ contracts/interfaces/ICirculatingMarketCapOracle.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.6.0;
 
 interface ICirculatingMarketCapOracle {
+  function isTokenWhitelisted(address token) external view returns (bool);
+
   function getCirculatingMarketCap(address) external view returns (uint256);
 
   function getCirculatingMarketCaps(address[] calldata) external view returns (uint256[] memory);
